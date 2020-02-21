e2c077198234dc20013076b6c66b28e311338def
Rawaa Al Kabbani <rawaa.alkabbani@outlook.com>
2020-02-21 15:35:23 +0100

    Fetch the categories from the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [categories, setCategories] = useState('');
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const categoriesResponse = await fetch('/getCategories');
+    const categoriesResponseJson = await categoriesResponse.json();
+    setCategories(categoriesResponseJson);
+  };
   return (
     <div className="App">
       <p>Hello zoom</p>
