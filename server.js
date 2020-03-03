467e2519a46534583f60fac134eacdd46a0b77a9
abdulrauf84 <abdul.rauf@live.se>
2020-03-03 23:42:23 +0100

    signin query added

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const database = require('./database.js');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 
 function makeQuery(query) {
   return new Promise((resolve, reject) => {
-    database.query(query, function(err, result, fields) {
+    database.query(query, function (err, result, fields) {
       if (err) {
         reject(err);
       } else {
@@ -21,7 +22,7 @@ async function getProducts() {
   return await makeQuery('SELECT * FROM product');
 }
 
-app.get('/getProducts', async function(request, response) {
+app.get('/getProducts', async function (request, response) {
   let products = await getProducts();
   response.send(products);
 });
@@ -30,7 +31,7 @@ async function getUsers() {
   return await makeQuery('SELECT * FROM user');
 }
 
-app.get('/getUsers', async function(request, response) {
+app.get('/getUsers', async function (request, response) {
   let users = await getUsers();
   response.send(users);
 });
@@ -39,7 +40,7 @@ async function getCategories() {
   return await makeQuery('SELECT * FROM category');
 }
 
-app.get('/getCategories', async function(request, response) {
+app.get('/getCategories', async function (request, response) {
   let categories = await getCategories();
   response.send(categories);
 });
@@ -48,7 +49,7 @@ async function getSubCategories() {
   return await makeQuery('SELECT * FROM sub_category');
 }
 
-app.get('/getSubCategories', async function(request, response) {
+app.get('/getSubCategories', async function (request, response) {
   let subCategories = await getSubCategories();
   response.send(subCategories);
 });
@@ -57,7 +58,7 @@ async function getCities() {
   return await makeQuery('SELECT * FROM city');
 }
 
-app.get('/getCities', async function(request, response) {
+app.get('/getCities', async function (request, response) {
   let cites = await getCities();
   response.send(cites);
 });
@@ -68,7 +69,7 @@ INNER JOIN zoom.sub_category ON zoom.product.subcategoryid = zoom.sub_category.s
 INNER JOIN zoom.category ON zoom.category.categoryid = zoom.sub_category.categoryid`);
 }
 
-app.post('/queryProducts', async function(request, response) {
+app.post('/queryProducts', async function (request, response) {
   const query = request.body.query.toLowerCase();
   let products = await getProductsForQuery();
   products = products.filter(product => {
@@ -79,4 +80,31 @@ app.post('/queryProducts', async function(request, response) {
   response.send(products);
 });
 
-app.listen(process.env.PORT || 5000);
+
+app.post('/signup', (request, response) => {
+  const sql = `INSERT INTO user (email, phonenumber) values ('${request.body.email}','${request.body.phonenumber}')`;
+  database.query(sql, (err, result) => {
+    if (err) console.log(err)
+    console.log(result)
+    result.effectedRowa > 0
+  })
+});
+
+app.get('/getuser', (request, response) => {
+
+  const email = request.query.email;
+  const password = request.query.password;
+
+  console.log(request.query.email)
+  console.log(request.query.password)
+
+  const sql = 'SELECT * FROM user WHERE email=? AND auth_str=?';
+  database.query(sql, [email, password], (err, result) => {
+    if (err) response.send(err)
+    console.log(result)
+    response.send(result)
+  })
+
+});
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
