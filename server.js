af094a73dc5a9d56da11b442f4c427aa10573ed5
Rawaa Al Kabbani <rawaa.alkabbani@outlook.com>
2020-03-08 10:07:42 +0100

    Edit getCities query and getProductsForQuery query fixes #8 fixes #9

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 function makeQuery(query) {
   return new Promise((resolve, reject) => {
-    database.query(query, function (err, result, fields) {
+    database.query(query, function(err, result, fields) {
       if (err) {
         reject(err);
       } else {
@@ -22,7 +22,7 @@ async function getProducts() {
   return await makeQuery('SELECT * FROM product');
 }
 
-app.get('/getProducts', async function (request, response) {
+app.get('/getProducts', async function(request, response) {
   let products = await getProducts();
   response.send(products);
 });
@@ -31,7 +31,7 @@ async function getUsers() {
   return await makeQuery('SELECT * FROM user');
 }
 
-app.get('/getUsers', async function (request, response) {
+app.get('/getUsers', async function(request, response) {
   let users = await getUsers();
   response.send(users);
 });
@@ -40,7 +40,7 @@ async function getCategories() {
   return await makeQuery('SELECT * FROM category');
 }
 
-app.get('/getCategories', async function (request, response) {
+app.get('/getCategories', async function(request, response) {
   let categories = await getCategories();
   response.send(categories);
 });
@@ -49,62 +49,63 @@ async function getSubCategories() {
   return await makeQuery('SELECT * FROM sub_category');
 }
 
-app.get('/getSubCategories', async function (request, response) {
+app.get('/getSubCategories', async function(request, response) {
   let subCategories = await getSubCategories();
   response.send(subCategories);
 });
 
 async function getCities() {
-  return await makeQuery('SELECT * FROM city');
+  return await makeQuery('SELECT zoom.city.cityname FROM city ORDER BY zoom.city.cityname ASC');
 }
 
-app.get('/getCities', async function (request, response) {
+app.get('/getCities', async function(request, response) {
   let cites = await getCities();
   response.send(cites);
 });
 
 async function getProductsForQuery() {
-  return await makeQuery(`SELECT zoom.product.title, zoom.product.description, zoom.sub_category.subcategoryname, zoom.category.categoryname FROM zoom.product
+  return await makeQuery(`SELECT zoom.product.title, zoom.product.description, zoom.sub_category.subcategoryname, zoom.category.categoryname, zoom.city.cityname FROM zoom.product
 INNER JOIN zoom.sub_category ON zoom.product.subcategoryid = zoom.sub_category.subcategoryid
-INNER JOIN zoom.category ON zoom.category.categoryid = zoom.sub_category.categoryid`);
+INNER JOIN zoom.category ON zoom.category.categoryid = zoom.sub_category.categoryid
+INNER JOIN zoom.user ON zoom.user.userid = zoom.product.userid
+INNER JOIN zoom.city ON zoom.city.cityid = zoom.user.cityid`);
 }
 
-app.post('/queryProducts', async function (request, response) {
+app.post('/queryProducts', async function(request, response) {
   const query = request.body.query.toLowerCase();
   let products = await getProductsForQuery();
-  products = products.filter(product => {
-    return Object.values(product).some(value => {
-      return value.toLowerCase().indexOf(query) !== -1;
+  if (query.length > 0) {
+    products = products.filter(product => {
+      return Object.values(product).some(value => {
+        return value.toLowerCase().indexOf(query) !== -1;
+      });
     });
-  });
+  }
   response.send(products);
 });
-
 
 app.post('/signup', (request, response) => {
   const sql = `INSERT INTO user (email, phonenumber) values ('${request.body.email}','${request.body.phonenumber}')`;
   database.query(sql, (err, result) => {
-    if (err) console.log(err)
-    console.log(result)
-    result.effectedRowa > 0
-  })
+    if (err) console.log(err);
+    console.log(result);
+    result.effectedRowa > 0;
+  });
 });
 
 app.get('/getuser', (request, response) => {
-
   const email = request.query.email;
   const password = request.query.password;
 
-  console.log(request.query.email)
-  console.log(request.query.password)
+  console.log(request.query.email);
+  console.log(request.query.password);
 
   const sql = 'SELECT * FROM user WHERE email=? AND auth_str=?';
   database.query(sql, [email, password], (err, result) => {
-    if (err) response.send(err)
-    console.log(result)
-    response.send(result)
-  })
-
+    if (err) response.send(err);
+    console.log(result);
+    response.send(result);
+  });
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
